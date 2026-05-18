08c86372b057b1f18c55ab802dbd474ee7cfe806
rayho6240@gmail.com <rayho6240@gmail.com>
2026-05-19 07:47:14 +0800

    為Sparky 新增圖片，但是現在有bug

--- Sparky.cpp
+++ Sparky.cpp
@@ -1,24 +1,50 @@
 #include "Sparky.h"
 #include <QPixmap>
+#include <QDebug>
 
 Sparky::Sparky(QGraphicsItem *player, QGraphicsItem *parent)
     : Enemy(parent), targetPlayer(player) {
 
-    QPixmap pix(60, 60);
-    pix.fill(Qt::red);
-    setPixmap(pix);
 
-    currentState = IDLE;
-    stateTimer = 0;
-    facingDirection = -1;
-    vx = 0;
+
+    // 定義一個公用的縮放大小，確保跟物理 handlePhysics 對齊
+        QSize spawnSize(60, 60);
+
+        // 載入並縮放左邊圖 1
+        leftImage1.load(":/Project2_Dataset/Image/Sparky/Sparky_left_1.png");
+        leftImage1 = leftImage1.scaled(spawnSize, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+        // 載入並縮放左邊圖 2
+        leftImage2.load(":/Project2_Dataset/Image/Sparky/Sparky_left_2.png");
+        leftImage2 = leftImage2.scaled(spawnSize, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+        // 載入並縮放右邊圖 1 (注意你的檔名拼錯 ritht)
+        rightImage1.load(":/Project2_Dataset/Image/Sparky/Sparky_ritht_1.png");
+        rightImage1 = rightImage1.scaled(spawnSize, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+        // 載入並縮放右邊圖 2
+        rightImage2.load(":/Project2_Dataset/Image/Sparky/Sparky_ritht_2.png");
+        rightImage2 = rightImage2.scaled(spawnSize, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+        // 🛡️ 防護罩：檢查圖片是否真的載入成功 (如果路徑錯了這裡會印出錯誤)
+        if (leftImage1.isNull()) qDebug() << "❌ 警告：Sparky 左側圖片載入失敗，請檢查資源路徑！";
+
+        // 2. 設定初始圖片
+        setPixmap(leftImage1);
+
+        // ... 其他初始化 ...
+        currentState = IDLE;
+        stateTimer = 0;
+        facingDirection = -1;
+        vx = 0;
+        frameCounter = 0;
+
 }
 
 void Sparky::update(){
     stateTimer++;
 
     if (currentState == IDLE) {
-        stateTimer++;
         vx = 0;
 
         if (stateTimer >= 60) {
@@ -39,7 +65,6 @@ void Sparky::update(){
             }
 
             vx = facingDirection * 3;
-            //TODO: 根據面對方向切換圖片
         }
     }
 
@@ -51,8 +76,6 @@ void Sparky::update(){
             currentState=IDLE;
             stateTimer=0;
             vx=0;
-
-            //TODO 換回IDLE圖
         }
     }
 
@@ -60,6 +83,34 @@ void Sparky::update(){
     if (!isDead){
         handlePhysics(60,60);
     }
+
+    updateSprite();
 };
+
+
+void Sparky::updateSprite() {
+    // 1. 讓動畫計數器持續增加
+    frameCounter++;
+
+    // 2. 計算動畫格數 (算出 0 或 1)
+    int animFrame = (frameCounter / 10) % 2;
+
+    // 3. 直接從記憶體拿取你在建構子已經準備好的圖片！不需要再讀取硬碟了！
+    if (facingDirection == -1) {
+        // 面朝左
+        if (animFrame == 0) {
+            setPixmap(leftImage1);
+        } else {
+            setPixmap(leftImage2);
+        }
+    } else {
+        // 面朝右
+        if (animFrame == 0) {
+            setPixmap(rightImage1);
+        } else {
+            setPixmap(rightImage2);
+        }
+    }
+}
 
 
--- Sparky.h
+++ Sparky.h
@@ -3,6 +3,7 @@
 
 #include "Enemy.h"
 #include <QGraphicsItem> // 確保有引入這個
+#include <QString>
 
 class Sparky: public Enemy{
 public:
@@ -20,7 +21,17 @@ private:
     // 【新增】：用來儲存卡比的指標
     QGraphicsItem *targetPlayer;
 
-};
+    void updateSprite();
+    int frameCounter = 0;
 
+
+    // 【新增】：用來儲存所有預載入好、縮放好的圖片
+    QPixmap leftImage1, leftImage2;
+    QPixmap rightImage1, rightImage2;
+
+    QPixmap attack1, attack2;
+
+
+};
 
 #endif // SPARKY_H
